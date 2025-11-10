0a0ab850d35203366f7d3b9db0ff4dedde535f05
michelledayana <michurivadeneira98@gmail.com>
2025-11-10 11:58:14 -0500

    🐳 Add Dockerfile to containerize the application

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from flask import Flask
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Esto carga las variables de .env
+# Cargar variables de entorno
+load_dotenv()
 
 SPACE_ID = os.getenv("CONTENTFUL_SPACE_ID")
 ACCESS_TOKEN = os.getenv("CONTENTFUL_ACCESS_TOKEN")
 
-print("SPACE_ID:", SPACE_ID)        # Para probar si se está leyendo
-print("ACCESS_TOKEN:", ACCESS_TOKEN)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"""
+    <h2>🚀 Proyecto desplegado correctamente</h2>
+    <p><b>SPACE_ID:</b> {SPACE_ID}</p>
+    <p><b>ACCESS_TOKEN:</b> {ACCESS_TOKEN[:10]}... (oculto)</p>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
